9d595414278148e91d4985caa05b7e49c9144723
Zaephor <Zaephor@users.noreply.github.com>
2017-06-28 19:26:34 -0700

    Furthering the base serf image and starting on a redis image

--- serf/docker-entrypoint.sh
+++ serf/docker-entrypoint.sh
@@ -1,2 +1,14 @@
 #!/bin/sh
+set -e
+
 join_serf.sh
+
+if [ -e '/usr/local/bin/include.sh' ]; then
+	source /usr/local/bin/include.sh
+fi
+
+if [ -e '/usr/local/bin/intermediate.sh' ]; then
+	/usr/local/bin/intermediate.sh
+fi
+
+exec "$@"
